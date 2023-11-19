682c9de7f32a5cdac79eaab0128757d08e90d619
RAVONDRAINIBE Geraldo <hei.ravo@gmail.com>
2023-11-19 08:12:41 +0300

    add a file Administrator

---
+++ src/main/java/hei/school/Administrator.java
@@ -0,0 +1,7 @@
+package hei.school;
+
+public class Administrator extends User{
+    public Administrator(int idUser, String name, String dateOfBirth, String email) {
+        super(idUser, name, dateOfBirth, email);
+    }
+}
