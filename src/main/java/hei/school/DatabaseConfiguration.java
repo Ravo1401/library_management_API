9e7bc81a333675c014c15e383bf43bb5de4ee3e3
RAVONDRAINIBE Geraldo <hei.ravo@gmail.com>
2023-11-19 08:44:35 +0300

    add file DatabaseConfiguration to connecting java in postgresql

---
+++ src/main/java/hei/school/DatabaseConfiguration.java
@@ -0,0 +1,33 @@
+package hei.school;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfiguration {
+    private static Connection connection;
+
+    private DatabaseConfiguration(){
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost/library_management_api",
+                    System.getenv("DB_USER"),
+                    System.getenv("DB_PASSWORD")
+            );
+            System.out.println("Successful connection");
+        } catch (SQLException e) {
+            System.out.println("Error while connecting to the database : " + e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() {
+        if (connection == null){
+            new DatabaseConfiguration();
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        DatabaseConfiguration.getConnection();
+    }
+}
