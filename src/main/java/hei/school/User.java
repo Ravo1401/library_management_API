d0c090ea0ef63a5d4ed0e78d22c312fe116abb4f
RAVONDRAINIBE Geraldo <hei.ravo@gmail.com>
2023-11-19 07:56:56 +0300

    my first commit

---
+++ src/main/java/hei/school/User.java
@@ -0,0 +1,15 @@
+package hei.school;
+
+public class User {
+    private int idUser;
+    private String name;
+    private String dateOfBirth;
+    private String email;
+
+    public User(int idUser, String name, String dateOfBirth, String email) {
+        this.idUser = idUser;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+    }
+}
