e8a45caf79b0c1af725bc8780e4d59e6078872b0
RAVONDRAINIBE Geraldo <hei.ravo@gmail.com>
2023-11-19 08:53:43 +0300

    add a file Author

---
+++ src/main/java/hei/school/Author.java
@@ -0,0 +1,13 @@
+package hei.school;
+
+public class Author {
+    private int authorId;
+    private String name;
+    private char sex;
+
+    public Author(int idauthor, String name, char sex) {
+        this.authorId = idauthor;
+        this.name = name;
+        this.sex = sex;
+    }
+}
